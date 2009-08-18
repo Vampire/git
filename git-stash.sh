08a9b74965e44261203e7b1f4d3e9797d81f144f
Jeff King <peff@peff.net>
2009-08-18 12:26:19 -0400

    stash apply -p WIP [ci skip]

--- git-stash.sh
+++ git-stash.sh
@@ -390,7 +390,7 @@ show_stash () {
 		fi
 	fi
 
-	git diff ${FLAGS} $b_commit $w_commit
+	git diff ${FLAGS} ${patch_mode:+-p} $b_commit $w_commit
 }
 
 show_help () {
@@ -442,6 +442,7 @@ parse_flags_and_rev()
 	IS_STASH_LIKE=
 	IS_STASH_REF=
 	INDEX_OPTION=
+	patch_mode=
 	s=
 	w_commit=
 	b_commit=
@@ -465,6 +466,13 @@ parse_flags_and_rev()
 			;;
 			--help)
 				show_help
+			;;
+			-p|--patch)
+				if test "$allow_patch_mode" = t; then
+					patch_mode=t
+				else
+					FLAGS="${FLAGS}${FLAGS:+ }$opt"
+				fi
 			;;
 			-*)
 				test "$ALLOW_UNKNOWN_FLAGS" = t ||
@@ -546,8 +554,19 @@ assert_stash_ref() {
 	}
 }
 
+munge_tree_interactive () {
+	(
+	GIT_INDEX_FILE="$TMP-index"
+	export GIT_INDEX_FILE
+	git read-tree --reset "$2" &&
+	git add--interactive --patch=stash-apply-$1 "$3" -- >&3 &&
+	git write-tree
+	)
+}
+
 apply_stash () {
 
+	allow_patch_mode=t
 	assert_stash_like "$@"
 
 	git update-index -q --refresh || die "$(gettext "unable to refresh index")"
@@ -560,12 +579,21 @@ apply_stash () {
 	if test -n "$INDEX_OPTION" && test "$b_tree" != "$i_tree" &&
 			test "$c_tree" != "$i_tree"
 	then
-		git diff-tree --binary $s^2^..$s^2 | git apply --cached
-		test $? -ne 0 &&
-			die "$(gettext "Conflicts in index. Try without --index.")"
-		unstashed_index_tree=$(git write-tree) ||
-			die "$(gettext "Could not save index tree")"
-		git reset
+		if test -n "$patch_mode"; then
+			i_tree=`munge_tree_interactive index $b_tree $i_tree` 3>&1
+		fi
+		if test "$b_tree" != "$i_tree"; then
+			git diff-tree --binary $b_tree $i_tree | git apply --cached
+			test $? -ne 0 &&
+				die "$(gettext "Conflicts in index. Try without --index.")"
+			unstashed_index_tree=$(git write-tree) ||
+				die "$(gettext "Could not save index tree")"
+			git reset
+		fi
+	fi
+
+	if test -n "$patch_mode"; then
+		w_tree=`munge_tree_interactive tree $b_tree $w_tree` 3>&1
 	fi
 
 	if test -n "$u_tree"
