a32f76ad5b98d67af925469b1a458c99e8d3aa03
Vicent Marti <tanoku@gmail.com>
2014-04-22 13:05:30 +0200

    ahead-behind: Add `--stdin` mode

--- builtin/ahead-behind.c
+++ builtin/ahead-behind.c
@@ -23,57 +23,75 @@ static struct object_array_entry *find_pending(struct object_array *array,
 	return NULL;
 }
 
+static struct commit *default_base = NULL;
+static const char *base_ref = "master";
+
+static void handle_arg(struct rev_info *revs, const char *revarg)
+{
+	struct object_array_entry *tip_ent, *base_ent;
+	struct commit *tip, *base;
+	int ahead, behind;
+
+	if (handle_revision_arg(revarg, revs, 0, REVARG_CANNOT_BE_FILENAME))
+		die("bad revision '%s'", revarg);
+
+	base_ent = find_pending(&revs->pending, UNINTERESTING, UNINTERESTING);
+	if (base_ent)
+		base = lookup_commit_reference(base_ent->item->oid.hash);
+	else if (default_base)
+		base = default_base;
+	else
+		base = default_base = lookup_commit_reference_by_name(base_ref);
+	if (!base)
+		die("missing base for ahead-behind stats");
+
+	tip_ent = find_pending(&revs->pending, 0, UNINTERESTING);
+	if (!tip_ent)
+		die("not an interesting ref: %s", revarg);
+	tip = lookup_commit_reference(tip_ent->item->oid.hash);
+	if (!tip)
+		die("failed to lookup commit ref %s", oid_to_hex(&tip_ent->item->oid));
+
+	revision_ahead_behind(tip, base, &ahead, &behind);
+	printf("%s %d %d\n", tip_ent->name, ahead, behind);
+
+	object_array_clear(&revs->pending);
+}
+
 int cmd_ahead_behind(int argc, const char **argv, const char *prefix)
 {
-	struct commit *default_base = NULL;
-	const char *base_ref = "HEAD";
+	int from_stdin = 0;
+
 	struct option ahead_behind_opts[] = {
 		OPT_STRING('b', "base", &base_ref, N_("base"), N_("base reference to process")),
+		OPT_BOOL( 0 , "stdin", &from_stdin, N_("read rev names from stdin")),
 		OPT_END()
 	};
 
 	struct rev_info revs;
-	struct strbuf line = STRBUF_INIT;
 
 	argc = parse_options(argc, argv, NULL, ahead_behind_opts,
 			     ahead_behind_usage, 0);
 
 	init_revisions(&revs, NULL);
 
-	while (strbuf_getline(&line, stdin) != EOF) {
-		struct object_array_entry *tip_ent, *base_ent;
-		struct commit *tip, *base;
-		int ahead, behind;
+	if (from_stdin) {
+		struct strbuf line = STRBUF_INIT;
 
-		if (!line.len)
-			break;
+		while (strbuf_getline(&line, stdin) != EOF) {
+			if (!line.len)
+				break;
 
-		if (handle_revision_arg(line.buf, &revs, 0, REVARG_CANNOT_BE_FILENAME))
-			die("bad revision '%s'", line.buf);
+			handle_arg(&revs, line.buf);
+		}
 
-		base_ent = find_pending(&revs.pending, UNINTERESTING, UNINTERESTING);
-		if (base_ent)
-			base = lookup_commit_reference(base_ent->item->oid.hash);
-		else if (default_base)
-			base = default_base;
-		else
-			base = default_base = lookup_commit_reference_by_name(base_ref);
-		if (!base)
-			return 1;
+		strbuf_release(&line);
+	} else {
+		int i;
 
-		tip_ent = find_pending(&revs.pending, 0, UNINTERESTING);
-		if (!tip_ent)
-			die("not an interesting ref: %s", line.buf);
-		tip = lookup_commit_reference(tip_ent->item->oid.hash);
-		if (!tip)
-			return 1;
-
-		revision_ahead_behind(tip, base, &ahead, &behind);
-		printf("%s %d %d\n", tip_ent->name, ahead, behind);
-
-		object_array_clear(&revs.pending);
+		for (i = 0; i < argc; ++i)
+			handle_arg(&revs, argv[i]);
 	}
-	strbuf_release(&line);
 
 	return 0;
 }
