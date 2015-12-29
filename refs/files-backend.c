0433b14a4c831a04a2a66dd779a2c336ab771af2
Jeff King <peff@peff.net>
2015-12-29 00:21:19 -0500

    create_symref: deprecate support for writing symbolic links
    Long ago, in 2fabd21 (Disable USE_SYMLINK_HEAD by default,
    2005-11-15), we switched git's default behavior to writing
    text symrefs instead of symbolic links. Any scripts
    accustomed to looking directly at .git/HEAD were updated to
    use `rev-parse` instead. The Linux kernel's setlocalversion
    script was one, and it was fixed in 117a93d (kbuild: Use git
    in scripts/setlocalversion, 2006-01-04).
    
    However, the problem still happened when bisecting the
    kernel; pre-117a93d would not build properly with a newer
    git, because they wanted to look directly at HEAD. To solve
    this, we added 9f0bb90 (core.prefersymlinkrefs: use symlinks
    for .git/HEAD, 2006-05-02), which lets the user turn on the
    old behavior, theoretically letting you bisect older kernel
    history.
    
    But there are a few complications with this solution:
    
      - packed-refs means you are limited in what you can do
        with .git/HEAD. If it is a symlink, you may `readlink`
        it to see where it points, but you cannot necessarily
        `cat .git/HEAD` to get the sha1, as the pointed-to ref
        may be packed.
    
        In particular, this means that the pre-117a93d kbuild
        script would sometimes work and sometimes not.
    
      - These days, we bisect on a detached HEAD. So during
        bisection, .git/HEAD _is_ a regular file with the
        sha1, and it works to `cat` it, whether or not you set
        core.preferSymlinkRefs.
    
    Such scripts will all be broken again if we move to
    alternate ref backends. They should have learned to use
    `rev-parse` long ago, and it is only bisecting ancient
    history that is a problem.
    
    Now that almost ten years have passed, it seems less likely
    that developers will bisect so far back in history. And
    moreover, this is but one of many possible problems
    developers run into when trying to build versions. The
    standard solution is to apply a "fixup" patch or other
    workaround while test-building the project, and that would
    work here, too.
    
    This patch therefore deprecates core.preferSymlinkRefs
    completely. There are a few reasons to want to do so:
    
      1. It drops some code that is probably exercised very
         rarely.
    
      2. The symlink code is not up to the same standards as the
         rest of the ref code. In particular, it is racy with
         respect to simultaneous readers and writers.
    
      3. If we want to eventually drop the symlink-reading code,
         this is a good first step to deprecating it.

--- refs/files-backend.c
+++ refs/files-backend.c
@@ -2216,6 +2216,18 @@ static int create_symref_locked(struct files_ref_store *refs,
 	return 0;
 }
 
+static const char symlink_deprecation_warning[] =
+"The core.preferSymlinkRefs configuration option has been\n"
+"deprecated and will be removed in a future version of Git. If your\n"
+"workflow or script depends on '.git/HEAD' being a symbolic link,\n"
+"it should be adjusted to use:\n"
+"\n"
+"        git rev-parse HEAD\n"
+"\n"
+"        git rev-parse --symbolic-full-name HEAD\n"
+"\n"
+"to get the sha1 or branch name, respectively.";
+
 static int files_create_symref(struct ref_store *ref_store,
 			       const char *refname, const char *target,
 			       const char *logmsg)
@@ -2225,6 +2237,9 @@ static int files_create_symref(struct ref_store *ref_store,
 	struct strbuf err = STRBUF_INIT;
 	struct ref_lock *lock;
 	int ret;
+
+	if (prefer_symlink_refs)
+		warning("%s", symlink_deprecation_warning);
 
 	lock = lock_ref_sha1_basic(refs, refname, NULL,
 				   NULL, NULL, REF_NODEREF, NULL,
